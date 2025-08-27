cf75d4d0c8d2503f5deccc2928e01d32e58a209f
Zach <100164907@ccisd.net>
2025-08-27 11:46:51 -0500

    Update Window.h

--- Window.h
+++ Window.h
@@ -1,7 +1,29 @@
-// Window.h
 #ifndef WINDOW_H
 #define WINDOW_H
 
-//Code to make a window
+#include <GLFW/glfw3.h>
 
-#endif
+// Struct to hold window info
+typedef struct {
+    GLFWwindow* window;
+    int width;
+    int height;
+    const char* title;
+} Window;
+
+// Create the window (returns 0 on success, -1 on failure)
+int Window_create(Window* win, int width, int height, const char* title);
+
+// Destroy the window
+void Window_destroy(Window* win);
+
+// Check if the window should close
+int Window_shouldClose(Window* win);
+
+// Poll window events
+void Window_pollEvents(Window* win);
+
+// Get the GLFWwindow pointer (for Vulkan surface creation etc.)
+GLFWwindow* Window_getGLFWwindow(Window* win);
+
+#endif // WINDOW_H
